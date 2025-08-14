e802aa6559177dc595798d5412ecd8716da64b13
akolnrekaj <anton.kolnrekaj@thefalcontech.com>
2025-08-14 15:51:23 +0200

    fix-2
    i've adjusted county list to be a grid with chakra

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,16 +1,29 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+
 const CountryList = () => {
+  const items = [
+    "Later I will transform this items to Chakra UI",
+    "Mock State1",
+    "Mock State2",
+    "Mock State3",
+    "Mock State4",
+    "Mock State1",
+  ];
   return (
-    <>
-      <div>CountryList</div>
-      <ul>
-        <li>Later i will transform this items to chakra ui</li>
-        <li>Mock State1</li>
-        <li>Mock State2</li>
-        <li>Mock Stat3</li>
-        <li>Mock State4</li>
-        <li>Mock State1</li>
-      </ul>
-    </>
+    <SimpleGrid columns={[2, null, 3]} gap="6">
+      {items.map((item, index) => (
+        <Box
+          key={index}
+          p={4}
+          bg="blue.50"
+          borderRadius="md"
+          shadow="md"
+          textAlign="center"
+        >
+          {item}
+        </Box>
+      ))}
+    </SimpleGrid>
   );
 };
 
